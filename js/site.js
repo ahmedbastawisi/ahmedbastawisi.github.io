3f1ff44773ea419c96c1ce16d515d4675177b337
ahmedbastawisi <ahmedbastawisi@outlook.com>
2023-05-01 11:57:35 +0300

    add dark theme

--- js/site.js
+++ js/site.js
@@ -1,3 +1,7 @@
+let color = localStorage.getItem('nav-theme');
+
+const page = document.querySelector('html').classList.add(color || "theme-light");
+
 let links = document.querySelectorAll('nav a');
 
 let load = (event) => {
@@ -9,19 +13,16 @@ let load = (event) => {
         item.classList.remove("background-color-body-medium");
     });
 
-    fetch(event.target.href).then(function (response) {
-        // The API call was successful!
+    fetch(event.target.pathname).then(function (response) {
         return response.text();
     }).then(function (html) {
-        // This is the HTML from our response as a text string
         document.querySelector('main').innerHTML = html;
         event.target.classList.add("background-color-body-medium");
-
     }).catch(function (err) {
-        // There was an error
         console.warn('Something went wrong.', err);
     });
 }
+
 let lnk = localStorage.getItem('nav-lnk');
 
 links.forEach((item) => {
@@ -29,3 +30,31 @@ links.forEach((item) => {
 });
 
 document.querySelector(`nav a[href="${lnk || '/overview.html'}"]`).click();
+
+let theme = (event) => {
+
+    const page = document.querySelector('html');
+
+    if (page.classList.contains("theme-light")) {
+
+        page.classList.remove("theme-light");
+        page.classList.add("theme-dark");
+
+        localStorage.setItem('nav-theme', "theme-dark");
+
+    } else if (page.classList.contains("theme-dark")) {
+
+        page.classList.add("theme-high-contrast");
+        page.classList.remove("theme-dark");
+
+        localStorage.setItem('nav-theme', "theme-high-contrast");
+
+    } else {
+        page.classList.add("theme-light");
+        page.classList.remove("theme-high-contrast");
+
+        localStorage.setItem('nav-theme', "theme-light");
+    }
+}
+
+document.querySelector('#theme').addEventListener('click', theme);
